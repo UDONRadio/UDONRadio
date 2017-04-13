a82b3a9fd2cbbc2a398b59c4249bab6eaeb27028
Theophile VALLEE <tvallee@student.42.fr>
2017-04-13 17:02:16 +0200

    add OpenSans font

--- src/components/Styles.tsx
+++ src/components/Styles.tsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export const P = styled.p`
+  font-family: 'Open Sans';
+  font-size: 2em;
   color: #a3a3a3;
 `;
 
 export const H1 = styled.h1`
+  font-family: 'Open Sans';
+  font-size: 2em;
   color: #5e5e5e;
 `;
-
-/*  OpenSans 24pts regular
-  OpenSans 24pts bold*/
