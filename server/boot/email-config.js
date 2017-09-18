8d440ef60bfa9388051d7b88e5c4ea5ba2778093
Theophile VALLEE <tvallee@student.42.fr>
2017-09-18 19:25:22 +0200

    added gridsend mail service boilerplate

---
+++ server/boot/email-config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var DataSource = require('loopback-datasource-juggler').DataSource;
+var dsSendGrid = new DataSource('loopback-connector-sendgrid', {
+		    api_key: process.env.SENDGRID_API_KEY
+});
+
+
+module.exports = function(app) {
+
+	app.models.Email.attachTo(dsSendGrid);		
+	setTimeout(function () {app.models.Email.send({
+			to: "theophile759@gmail.com",
+			from: "webmaster@theof.fr",
+			subject: "Test",
+			text: "Text message",
+			html: "html <b>message</b>"
+	}, function(err, result) {
+	    if(err) {
+			        console.log('Upppss something crash', err);
+			        return;
+			    }
+	    console.log(result);
+	});}, 5000);
+};
