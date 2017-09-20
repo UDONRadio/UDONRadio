f54fba6cb2caa00581cda0539ea7da2a74d3a2af
LabElec <labelec@mac-labelec-02.42.fr>
2017-09-20 17:19:20 +0200

    getting to work with Modals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/lib/Button';
+import Modal from 'react-bootstrap/lib/Modal';
+
+class RuleModal extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {showModal: false};
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
+  }
+
+  render () {
+    return (
+      <div>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>TrackUpload</Modal.Title>
+          </Modal.Header>
+        </Modal>
+        <Button onClick={this.open}>Upload</Button>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <Button onClick={alert}>Click here !</Button>
+      <RuleModal/>
     );
   }
 }
