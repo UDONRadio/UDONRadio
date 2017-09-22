9d1031715b005fd5185d3df734a559efd19372e4
itheof <tvallee@student.42.fr>
2017-09-22 02:52:03 +0200

    switch to semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div>
           <PlayerPanel/>
+          Truc
           <UserManager/>
       </div>
     );
--- src/PlayerPanel.js
+++ src/PlayerPanel.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
+import { Icon, Menu, Button, Container} from 'semantic-ui-react';
+
+const PlayPauseButton = (props) => (
+  <Button style={{"background": "transparent"}} onClick={props.onClick}>
+    <Icon name={props.playing ? 'pause': 'play'} size='big'/>
+  </Button>
+)
 
 class PlayerPanel extends Component {
 
   constructor (props) {
     super(props);
+    this.state = {
+      playing: false
+    };
+    this.play = this.play.bind(this);
+    this.pause = this.pause.bind(this);
+  }
+
+  play () {
+    this.setState({
+      playing: true
+    });
+  }
+
+  pause () {
+    this.setState({
+      playing: false
+    });
   }
 
   render () {
-    const headerStyle = {
-      width: '100%',
-      height: '60px',
-      position: 'absolute',
-      left: 0,
-      top: 0,
-      'background-color': 'rgb(0, 0, 0)',
-      'z-index': 1000,
-    };
-    return <header style={headerStyle}>NotImplemented</header>
+    return <Menu fixed='top'>
+    <Container>
+      <PlayPauseButton
+        playing={this.state.playing}
+        onClick={this.state.playing ? this.pause : this.play}
+      />
+    </Container>
+    </Menu>
   }
 }
 
