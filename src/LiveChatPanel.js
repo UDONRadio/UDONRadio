846e18ee49ccfd4a4153d0902d5e7c3f18f98473
itheof <tvallee@student.42.fr>
2017-09-26 12:59:15 +0200

    integrated socket-io chat

--- src/LiveChatPanel.js
+++ src/LiveChatPanel.js
@@ -1,5 +1,37 @@
 import React, { Component } from 'react';
-import { Container, Form, Button, TextArea, Divider, List} from 'semantic-ui-react';
+import { Container, Button, TextArea, Divider, List} from 'semantic-ui-react';
+import * as io from 'socket.io-client';
+
+const temp_messages = [
+  {
+    'user': 'dodokilleurz',
+    'content': '<alert>Haha g hacker votre site 2 merd on va voir qui c le plu fort</alert>'
+  },
+  {
+    'user': 'theof',
+    'content': 'salut'
+  },
+  {
+    'user': 'remi',
+    'content': 'TECHNO (toujours pareil)...'
+  },
+  {
+    'user': 'remi',
+    'content': 'TECHNO (toujours pareil)...'
+  },
+  {
+    'user': 'remi',
+    'content': 'TECHNO (toujours pareil)...'
+  },
+  {
+    'user': 'remi',
+    'content': 'TECHNO (toujours pareil)...'
+  },
+  {
+    'user': 'remi',
+    'content': 'TECHNO (toujours pareil)...'
+  },
+];
 
 const ChatMessages = (props) => {
 
@@ -12,39 +44,92 @@ const ChatMessages = (props) => {
     </List.Item>
   );
 
-  return <List style={{'maxHeight': 'calc(100% - 150px)', 'overflowY': 'scroll'}}>
+  return <List style={{'maxHeight': 'calc(100% - 150px)', 'overflowY': 'auto'}}>
     {props.messages.map(makeMessage)}
   </List>
 }
 
 const ChatInput = (props) => {
 
-  const sendMessage = (value) => {
-    console.log(value);
-  };
-
   if (props.logged_in)
     var placeholder = "Allez, viens tchatcher !";
   else
     var placeholder = "Register to start chatting !";
 
-  //TODO: Hitting enter while focusing TextArea sends the form
-  return <div style={{'bottom': '0px', 'width':'100%', 'position':'absolute'}}>
-    <Form>
-    <Form.Field>
-      <TextArea placeholder={placeholder} style={{'resize':'none', 'paddingRight':'14px'}} />
-    </Form.Field>
+  return <div style={{'bottom': '0px', 'width':'100%', 'position':'absolute',}}>
+    <form onSubmit={props.onSubmit}>
+    <TextArea
+      placeholder={placeholder}
+      value={props.value}
+      onChange={props.onChange}
+      style={{'resize':'none', 'padding':'14px', 'width':'100%'}}
+    />
     <Button>Like</Button>
     <Button type='submit' style={{'float':'right'}}>Envoyer</Button>
-    </Form>
+    </form>
   </div>
 }
 
-const LiveChatPanel = (props) => (
-  <div style={{'height':'100%', 'position': 'relative'}}>
-    <ChatMessages messages={props.messages}></ChatMessages>
-    <ChatInput logged_in/>
-  </div>
-)
+class LiveChatPanel extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      'messages' : temp_messages,
+      'socket': io('http://localhost:3001/'),
+      'username': '',
+      'text': '',
+    };
+    this.state.socket.on('change username', function (username) {
+      this.setState({
+        username: username
+      });
+    }.bind(this));
+    this.state.socket.on('chat message', function (name, text) {
+      this.setState({
+        messages: this.state.messages.concat([{user:name, content:text}])
+      })
+    }.bind(this))
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //TODO: Hitting enter while focusing TextArea sends the form
+  handleChange (event) {
+    this.setState({text: event.target.value});
+  }
+
+  handleSubmit (event) {
+    event.preventDefault();
+    if (this.state.text == '')
+      return ;
+    if (this.state.username) {
+      this.state.socket.emit('chat message', this.state.text);
+      this.setState({
+        text: ''
+      });
+    }
+    else {
+      this.state.socket.emit('change username', this.state.text);
+      this.setState({
+        text: '',
+        username: this.state.text,
+      });
+    }
+  }
+
+  render () {
+    return <div style={{'height':'100%', 'position': 'relative'}}>
+      <ChatMessages messages={this.state.messages}></ChatMessages>
+      <ChatInput
+        logged_in={this.state.username != ''}
+        onChange={this.handleChange}
+        onSubmit={this.handleSubmit}
+        value={this.state.text}
+      />
+    </div>
+  }
+}
 
 export default LiveChatPanel;
