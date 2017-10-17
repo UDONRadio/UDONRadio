cc082955e46c534ee04474dcc3290128b4cf2b29
itheof <tvallee@student.42.fr>
2017-10-18 01:02:21 +0200

    email is now required for users

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_('email address'))
+    REQUIRED_FIELDS = ['email',]
