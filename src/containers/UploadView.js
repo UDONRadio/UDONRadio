8f29147e61cf4759fe46fb89bf9bb983a2d6189b
LabElec <labelec@mac-labelec-02.42.fr>
2017-10-20 01:23:01 +0200

    progress on upload

--- src/containers/UploadView.js
+++ src/containers/UploadView.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import { Divider, Dimmer, Icon } from 'semantic-ui-react';
+import { Divider, Dimmer, Icon, Message } from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
 
-const FileUploader = (props) => (
-  <div>
-    <Dropzone>
-      <Icon name='upload' size='massive'/>
-    </Dropzone>
-  </div>
-)
+class FileUploader extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      last_rejected : [],
+    }
+  }
+
+  onDrop = (acceptedFiles, rejectedFiles) => {
+    console.log(acceptedFiles)
+    console.log('accepted: ' + acceptedFiles)
+    console.log('rejected: ' + rejectedFiles)
+    this.setState({last_rejected: rejectedFiles})
+  }
+
+  render () {
+    return <div>
+      <Dropzone onDrop={this.onDrop} accept="audio/*">
+        <Icon name='upload' size='massive'/>
+      </Dropzone>
+      <Message warning visible={this.state.last_rejected.length} list={
+        this.state.last_rejected
+      } header='Some files were ommited'/>
+    </div>
+  }
+}
+
 
 const UploadStatus = (props) => (
   <a></a>
@@ -23,7 +44,6 @@ class UploadView extends Component {
   constructor () {
     super()
     this.state = {}
-
   }
 
   render () {
