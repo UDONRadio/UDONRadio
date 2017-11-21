643f001ff3a5deda6108fb0277f49be34d493bad
Theophile VALLEE <tvallee@student.42.fr>
2017-11-21 03:43:44 +0100

    Liquidsoap integration (#3)
    * added liquidsoap and icecast with basic conf
    
    * use our own icecast container
    
    * more icecast config and fixed startup
    
    * rights management

---
+++ icecast/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Expand the template conf file with environment variables
+python3 << EOF
+import os
+with open("/config/icecast.xml.template", "r") as src:
+    with open("/config/icecast.xml", "w") as dst:
+        dst.write(src.read().format(**os.environ))
+EOF
+
+chown icecast2 /config/icecast.xml
+# Hand over to icecast
+su icecast2 -s "/bin/bash" -c "/usr/bin/icecast2 -c /config/icecast.xml"
+yes > /dev/null
