a9eb38e713f736654d12305c95c4a70351189f95
itheof <tvallee@student.42.fr>
2017-12-17 22:54:30 +0100

    admin site models

--- radio/admin.py
+++ radio/admin.py
@@ -2,4 +2,23 @@ from django.contrib import admin
 from .models import AutoPlaylist, AutoPlaylistIndex, Song
 
 admin.site.register(AutoPlaylist)
-admin.site.register(Song)
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+
+    date_hierarchy = 'upload__created'
+    list_display = (
+        'title',
+        'artist',
+        'album',
+        'length',
+        'play_count',
+        'uploaded_by',
+        'uploaded_on'
+    )
+
+    def uploaded_by(self, obj):
+        return obj.upload.up_by
+
+    def uploaded_on(self, obj):
+        return obj.upload.created
--- udon_back/admin.py
+++ udon_back/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'is_adherent'
+    )
