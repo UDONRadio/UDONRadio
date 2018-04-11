3e23deac656ad27a98dadc63e9e8db6320541cd3
itheof <tvallee@student.42.fr>
2018-04-11 17:52:06 +0200

    back: advanced ws url routing

--- back/chat/routing.py
+++ back/chat/routing.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import consumers
 
+#TODO: https://github.com/django/channels/issues/870 make the urlpattern
+#      non project aware.
 websocket_urlpatterns = [
     url(r'^ws/chat/$', consumers.ChatConsumer),
 ]
--- back/udon_back/routing.py
+++ back/udon_back/routing.py
@@ -1,12 +1,17 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
 import chat.routing
+
+ws_router = URLRouter([
+    url(r'^ws/chat/', URLRouter(chat.routing.websocket_urlpatterns)),
+])
 
 application = ProtocolTypeRouter({
     # http->django views is added by default
     'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            url(r'^ws/', ws_router),
+        ])
     ),
 })
