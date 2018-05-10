UDONRadio/UDONRadio

commit 73a743194850b7868fd3b9df024b5a8dd4ef6a08

front: fix request function for previously authenticated users
Authentication would be sent even if the user logged out (which destroys the token), causing 401 errors.

front/src/containers/UserManager.js
@@ -111,7 +111,8 @@ class UserManager extends Component {
 
   /* Make authenticated requests */
   request = (url, data) => {
-    data.headers.Authorization = 'Token ' + this.state.auth_token;
+    if (this.state.logged_in)
+      data.headers.Authorization = 'Token ' + this.state.auth_token;
     return request(url, data);
   }
 

commit 2dc710a6f6fe67989b2db2c7fb57179ff05660cd

front: fixed broken login introduced by 73a7431

front/src/containers/UserManager.js
@@ -15,7 +15,7 @@ class UserManager extends Component {
     const auth_token = localStorage.getItem('auth_token')
     this.state = {
       showLoginRegisterModal: this.showLoginRegisterModal,
-      logged_in: false,
+      logged_in: (auth_token !== null) ? true : false,
       is_staff: false,
       is_adherent: false,
       auth_token: auth_token,
@@ -91,8 +91,9 @@ class UserManager extends Component {
       .then((response) => {
         localStorage.setItem('auth_token', response.auth_token)
         this.setState({
-          'auth_token': response.auth_token,
-          '__showModal': false
+					'auth_token': response.auth_token,
+					'logged_in': true,
+          '__showModal': false,
         }, this.getUserInfo);
       })
       .catch(onError);