308ffe5e543bcba5ba5d55aa5a2cb375bb58c704
itheof <tvallee@student.42.fr>
2018-05-20 15:02:43 +0200

    back: fix liquidsoap bridge db

--- back/udon_back/settings.py
+++ back/udon_back/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} if DEBUG and not LIQUIDSOAP_BRIDGE else {
+} if DEBUG else {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
