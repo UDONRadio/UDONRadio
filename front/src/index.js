7f28453d14f92475a7e9cf2e49b8e2d546b8bb93
theof <tvallee@student.42.fr>
2018-06-01 19:10:31 +0000

    front: unregister service worker to make cache great again

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { unregister } from './registerServiceWorker';
+
+unregister();
 /*
 does not seem really necessary
 import registerServiceWorker from './registerServiceWorker';
