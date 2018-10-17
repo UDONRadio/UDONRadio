954a9baffeba2f7ef0519375b3c39e29ddd10fc3
itheof <tvallee@student.42.fr>
2018-10-17 12:30:58 +0200

    back use secret key from env (fixes #38)

--- back/udon_back/settings.py
+++ back/udon_back/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@86n*v3omab*1ata8mu+&6w*)xd=x3o_=hr1plh6*-4e_&7g6^'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('PRODUCTION', '') != 'True' else False
