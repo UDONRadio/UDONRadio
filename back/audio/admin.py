7bde7f6948da18f44b3daab09c07976b8b19b5dd
itheof <tvallee@student.42.fr>
2018-10-29 16:54:38 +0100

    back: show audio base_name in admin listing

--- back/audio/admin.py
+++ back/audio/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Audio
 
-admin.site.register(Audio)
+@admin.register(Audio)
+class AudioAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'base_name',
+        'assigned'
+    )
+
+    def assigned(self, obj):
+        dic = {}
+        for related in Audio.CONTENT_RELATED_FIELDS:
+            dic[related] = None
+        return obj in Audio.objects.filter(**dic)
