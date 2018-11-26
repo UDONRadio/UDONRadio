a6d2691e3bbf71026288dc45f22f493c1c4f9f88
itheof <tvallee@student.42.fr>
2018-11-26 10:44:34 +0100

    back: make User email unique and enforce ascii username

--- back/udon_back/models.py
+++ back/udon_back/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-    email = models.EmailField(_('email address'))
+    email = models.EmailField(_('email address'), unique=True)
+    username_validator = ASCIIUsernameValidator()
     REQUIRED_FIELDS = ['email',]
 
     @property
