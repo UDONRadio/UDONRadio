27812ad5fb69a4e27dd2b1b4477407576679193e
itheof <tvallee@student.42.fr>
2018-11-26 10:45:10 +0100

    back: drop session authentication for DRF

--- back/udon_back/settings.py
+++ back/udon_back/settings.py
@@ -63,7 +63,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
