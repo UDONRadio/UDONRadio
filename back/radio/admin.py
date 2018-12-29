9843a929eeb0ed1c1a1f9ecff41bd819607c667a
itheof <tvallee@student.42.fr>
2018-12-29 20:28:13 +0100

    back: add a button to redirect to webcaster in django admin for liveshows

--- back/radio/admin.py
+++ back/radio/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import AutoPlaylist, Song, LiveStream
+from django.utils.html import mark_safe
 
 admin.site.register(AutoPlaylist)
 
@@ -15,7 +16,7 @@ class SongAdmin(admin.ModelAdmin):
         'length',
         'play_count',
         'uploaded_by',
-        'uploaded_on'
+        'uploaded_on',
     )
 
     def uploaded_by(self, obj):
@@ -28,8 +29,15 @@ class SongAdmin(admin.ModelAdmin):
 class LiveStream(admin.ModelAdmin):
     list_display = (
         'host',
-        'title'
+        'title',
+        'show_webcaster_url'
     )
     readonly_fields = (
         'password',
     )
+
+    def show_webcaster_url(self, obj):
+        return mark_safe('<a target="_blank"' +
+        ' rel="noopener noreferrer"' +
+        ' style="color:white;background-color:red;padding-top:6px;padding-bottom:6px;padding-left:10px;padding-right:10px;border-radius:40px;"' +
+        ' href=http://webcaster.udonradio.fr/?user={}&password={}>Start streaming now !</a>'.format(obj.host, obj.password))
